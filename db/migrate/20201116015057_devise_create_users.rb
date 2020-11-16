d6a1ef15617e1f30845652f1fc60aa48ec846dab
yuko1117 <eyonseat@yahoo.co.jp>
2020-11-16 16:35:14 +0900

    usersテーブル

--- db/migrate/20201116015057_devise_create_users.rb
+++ db/migrate/20201116015057_devise_create_users.rb
@@ -4,14 +4,14 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
   def change
     create_table :users do |t|
       ## Database authenticatable
-      t.string :nickname,            null: false
       t.string :email,               null: false, default: ""
       t.string :encrypted_password,  null: false, default: ""
+      t.string :nickname,            null: false
       t.string :first_name,          null: false
       t.string :last_name,           null: false
       t.string :first_name_katakana, null: false
       t.string :last_name_katakana,  null: false
-      t.date :birth_date,            null: false
+      t.date   :birth_date,          null: false
 
 
       ## Recoverable
