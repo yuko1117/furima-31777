0b6ecd3f5e4da53349daa25cddd419793c5fc6b4
yuko1117 <eyonseat@yahoo.co.jp>
2020-11-19 09:27:44 +0900

    ユーザー新規登録・ログイン機能実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -8,5 +8,11 @@ class ItemsController < ApplicationController
 
   def create
   end
+
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+     redirect_to root_path
+  end
   
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,9 +5,10 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   
   validates :nickname, presence: true
-  validates :first_name, presence: true
-  validates :last_name, presence: true
-  validates :first_name_katakana, presence: true
-  validates :last_name_katakana, presence: true
+  validates :password, format: {with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i, message: 'Include both letters and numbers' }
+  validates :first_name, presence: true, format: {with: /\A(?:\p{Hiragana}|\p{Katakana}|[ー－]|[一-龠々])+\z/, message: 'Full-width characters'  }
+  validates :last_name, presence: true, format: {with: /\A(?:\p{Hiragana}|\p{Katakana}|[ー－]|[一-龠々])+\z/, message: 'Full-width characters'}
+  validates :first_name_katakana, presence: true, format: { with:  /\A[ァ-ヶー－]+\z/, message: 'Full-width katakana characters'} 
+  validates :last_name_katakana, presence: true , format: { with: /\A[ァ-ヶー－]+\z/, message: 'Full-width katakana characters'}
   validates :birth_date, presence: true
 end
