59ee5036c36f08fba8bcb7fc1d8e8878dc32f08a
yuko1117 <eyonseat@yahoo.co.jp>
2020-11-22 17:08:03 +0900

    販売手数料、販売利益の表示

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -1,3 +1,20 @@
-window.addEventListener('load', () => {
-  console.log("OK");
-});
+window.addEventListener("load",()=>{
+
+const priceInput = document.getElementById("item-price");
+priceInput.addEventListener("input", () => {
+  const inputValue = priceInput.value;
+  console.log(inputValue);
+
+  const addTaxDom = document.getElementById("add-tax-price");
+  addTaxDom.innerHTML = (Math.floor(inputValue * 0.1));
+    console.log(addTaxDom);
+
+  const profitNumber = document.getElementById("profit")
+  const addTaxValue=(Math.floor(inputValue * 0.1));
+  addTaxDom.innerHTML = addTaxValue;
+    profitNumber.innerHTML = (Math.floor(inputValue - addTaxValue));
+      console.log(profitNumber);
+  })
+})
+
+
