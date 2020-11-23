6ffe50fc934614d08901a0e5d123afcffae78c57
yuko1117 <eyonseat@yahoo.co.jp>
2020-11-23 14:25:05 +0900

    商品情報表示

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -17,6 +17,11 @@ class ItemsController < ApplicationController
     end
   end
 
+  def show
+    @item = Item.find(params[:id])
+    @user = User.find(@item.user_id)
+  end
+    
   private
 
   def item_params
