5be8cb07486c53e88081de93a077ae0af6f609d2
yuko1117 <eyonseat@yahoo.co.jp>
2020-12-02 12:38:47 +0900

    Update purchases_controller.rb
    修正

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,7 +1,7 @@
 class PurchasesController < ApplicationController
-  before_action :authenticate_user!, only: [:index]
+  before_action :authenticate_user!, only: [:index,:create]
   before_action :set_item, only: [:index, :create]
-  before_action :move_to_purchase, only: [:index, :create]
+  before_action :move_to_purchase, only: [:index]
 
   def index
     @item_purchase = ItemPurchase.new
